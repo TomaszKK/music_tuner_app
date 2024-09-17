c54a9814a647074fc200928eaf952bec93821334
TomaszKK <242136@edu.p.lodz.pl>
2024-09-17 15:44:54 +0200

    Add gesture detector with long press blocker on PinNoteWidget

--- lib/widgets/PinNoteWidget.dart
+++ lib/widgets/PinNoteWidget.dart
@@ -4,70 +4,69 @@ import 'package:music_tuner/widgets/NoteRepresentationWidget.dart';
 
 import 'TunerWidget.dart';
 
-class PinNoteWidget extends StatelessWidget {
-  const PinNoteWidget({Key? key, required this.defaultNote, required this.circleSize}) : super(key: key);
+class PinNoteWidget extends StatefulWidget {
+  const PinNoteWidget({
+    Key? key,
+    required this.defaultNote,
+    required this.circleSize,
+  }) : super(key: key);
 
   final String defaultNote;
   final double circleSize;
 
   @override
+  State<PinNoteWidget> createState() => _PinNoteWidgetState();
+}
+
+class _PinNoteWidgetState extends State<PinNoteWidget> {
+  bool isTapped = false; // State to track if the widget is tapped
+
+  @override
   Widget build(BuildContext context) {
-    return  ValueListenableBuilder<String>(
+    final themeColor = ThemeManager().currentTheme.colorScheme.secondaryContainer;
+
+    return ValueListenableBuilder<String>(
       valueListenable: TunerWidget.currentNoteNotifier, // Listen to the ValueNotifier
       builder: (context, currentNote, child) {
-        if (currentNote == defaultNote) {
-          return Container(
+        final isCurrentNote = currentNote == widget.defaultNote;
+
+        return GestureDetector(
+          onLongPress: () {
+            setState(() {
+              isTapped = !isTapped; // Toggle the tapped state
+            });
+          },
+          onTap: () {
+
+          },
+          child: Container(
             alignment: Alignment.center,
-            height: circleSize,
-            width: circleSize,
+            height: widget.circleSize,
+            width: widget.circleSize,
             decoration: BoxDecoration(
-              color: Colors.transparent,
+              color: isTapped ? themeColor : Colors.transparent, // Fill with red if tapped
               shape: BoxShape.circle,
               border: Border.all(
-                color: ThemeManager().currentTheme.colorScheme
-                    .secondaryContainer,
+                color: themeColor,
                 width: 2,
               ),
-              boxShadow: [
+              boxShadow: isCurrentNote
+                  ? [
                 BoxShadow(
-                  color: ThemeManager().currentTheme.colorScheme
-                      .secondaryContainer,
+                  color: themeColor,
                   blurRadius: 15.0,
-                  // spreadRadius: 3.0,
                   blurStyle: BlurStyle.outer,
                 ),
-                // BoxShadow(
-                //   color: ThemeManager().currentTheme.colorScheme
-                //       .secondaryContainer,
-                //   blurRadius: 10.0,
-                //   // spreadRadius: -5.0,
-                //   blurStyle: BlurStyle.inner,
-                // )
-              ],
+              ]
+                  : null, // Only add boxShadow if it's the current note
             ),
             child: NoteRepresentationWidget(
-                noteString: currentNote, fontSize: 20),
-          );
-        }
-        else {
-          return Container(
-            alignment: Alignment.center,
-            height: circleSize,
-            width: circleSize,
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-              shape: BoxShape.circle,
-              border: Border.all(
-                color: ThemeManager().currentTheme.colorScheme
-                    .secondaryContainer,
-                width: 2,
-              ),
+              noteString: isCurrentNote ? currentNote : widget.defaultNote,
+              fontSize: 20,
             ),
-            child: NoteRepresentationWidget(
-                noteString: defaultNote, fontSize: 20),
-          );
-        }
-      }
+          ),
+        );
+      },
     );
   }
 }
